fe2bdf50a83dbb8201e6291c4d2402e7873d9777
luka dodelia <luka@lnh.ge>
2026-01-26 15:00:25 +0400

    inter fonts

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+      },
       colors: {
         brand: "#FD4F00",
         hubspot: {
